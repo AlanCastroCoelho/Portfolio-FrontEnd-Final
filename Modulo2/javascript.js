5075dddbaf90900f6567c19b409d164ca112155a
Alan Castro <alancastrocoelhho@gmail.com>
2022-09-29 00:22:07 -0300

    Nav Bar con Nuevo Efecto

--- Modulo2/javascript.js
+++ Modulo2/javascript.js
@@ -1,4 +1,20 @@
-//////////// NAV ANIMATION /////////////
+//////////// NAV ANIMATION TEXT /////////////
+const menuItems = [...document.querySelectorAll('.menu-item')];
+menuItems.forEach(item => {
+
+  let word = item.children[0].children[0].innerText.split('');
+  item.children[0].innerHTML = '';
+  word.forEach((letter, idx) => {
+    item.children[0].innerHTML += `<span style="--index: ${idx};color:white">${letter}</span>`;
+  })
+let cloneDiv = item.children[0].cloneNode(true);
+cloneDiv.style.position = 'absolute';
+cloneDiv.style.left = '0'
+cloneDiv.style.top = '0';
+item.appendChild(cloneDiv)
+})
+
+//////////// NAV ANIMATION HAMBURGUER FOR MOBILE /////////////
 
 const navSlide = () => {
   const burguer = document.querySelector('.burguer');
