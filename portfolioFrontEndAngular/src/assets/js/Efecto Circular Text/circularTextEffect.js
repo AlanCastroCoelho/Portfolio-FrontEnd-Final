4cff1b63f6e7d2fb2b180eb9133ae54a6228916c
Alan Castro <alancastrocoelhho@gmail.com>
2023-04-13 22:58:43 -0300

    Imagenes e Iconos

---
+++ portfolioFrontEndAngular/src/assets/js/Efecto Circular Text/circularTextEffect.js
@@ -0,0 +1,12 @@
+/////// Circular Text- Hero Section - Start Code ///////
+
+const text = document.querySelector(".text p");
+text.innerHTML = text.innerText
+  .split("")
+  .map(
+    (char, i) => `
+  <span style="transform:rotate(${i * 3}deg)">${char}</span>`
+  )
+  .join("");
+
+/////// Circular Text - Hero Section - End Code ///////
