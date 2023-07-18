305ea364afe9bd6cde1d67cd8f66b3489e159a25
Alan Castro Coelho <109250693+AlanCastroCoelho@users.noreply.github.com>
2023-07-18 20:01:36 -0300

    Haciendo el effecto de las letras del nav directamente en el component.

--- portfolioFrontEndAngular/src/app/components/nav/nav.component.ts
+++ portfolioFrontEndAngular/src/app/components/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenService } from 'src/app/services/token.service';
 import { CargarScriptsService } from './../../services/cargar-scripts.service';
@@ -8,16 +8,15 @@ import { CargarScriptsService } from './../../services/cargar-scripts.service';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit , AfterViewInit{
+  @ViewChildren('menuItems') menuItems!: QueryList<ElementRef>;
   isLogged = false;
   openMenu = false;
 
   constructor(
     private router: Router,
     private tokenService: TokenService,
-    private _CargarScripts: CargarScriptsService
   ) {
-    _CargarScripts.Carga(['Efecto Letras/javascript']);
   }
 
   ngOnInit(): void {
@@ -52,4 +51,26 @@ export class NavComponent implements OnInit {
       element.scrollIntoView({ behavior: 'smooth' });
     } 
   }
+
+  ngAfterViewInit(): void {
+    this.letterEffect();
+   }
+ 
+   letterEffect():void{
+    this.menuItems.forEach(item => {
+      let word = item.nativeElement.children[0].children[0].innerText.split('');
+      item.nativeElement.children[0].innerHTML = '';
+      word.forEach((letter: string, idx: number) => {
+        item.nativeElement.children[0].innerHTML += `<span style="--index: ${idx};color:white">${letter}</span>`;
+      })
+  
+      let cloneDiv = item.nativeElement.children[0].cloneNode(true);
+  
+      cloneDiv.style.position = 'absolute';
+      cloneDiv.style.top = '0';
+      cloneDiv.style.width = '100%';
+      item.nativeElement.appendChild(cloneDiv);
+    });
+  }
+
 }
